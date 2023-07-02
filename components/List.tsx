97369b3f05ea8b542ac62a50d29827162081b85b
Luna <luni.siaa@gmail.com>
2023-07-02 22:50:54 +0200

    improve tab list

--- components/List.tsx
+++ components/List.tsx
@@ -1,4 +1,5 @@
-import React, { FunctionComponent } from "react";
+import { useRouter } from "next/navigation";
+import React, { FunctionComponent, useState } from "react";
 
 interface Props {
     tabs: {
@@ -6,17 +7,27 @@ interface Props {
         icon?: React.ReactElement;
         value: string;
     }[];
-    stateHook: React.Dispatch<React.SetStateAction<string>>;
-    state: string;
+    url: string;
 }
 
-export const ListTab: FunctionComponent<Props> = ({ tabs, stateHook, state }) => {
+export const ListTab: FunctionComponent<Props> = ({ tabs, url }) => {
+    const [state, setState] = useState<string>("/");
+    const router = useRouter();
+
     return (
-        <div className="text-sm font-medium text-center border-b border-wamellow-light">
+        <div className="text-sm font-medium text-center border-b border-wamellow-light my-3">
             <ul className="flex flex-wrap -mb-px">
                 {tabs.map((tab) => (
                     <li className="mr-2" key={tab.name}>
-                        <button className={`inline-block p-4 border-b-2 border-transparent rounded-t-lg ${state === tab.value ? "text-violet-500 border-b-2 border-violet-500" : ""} hover:text-violet-400 duration-200 drop-shadow-lg`} onClick={() => stateHook(tab.value)}>{tab.name}</button>
+                        <button
+                            className={`inline-block p-3 border-b-2 border-transparent rounded-t-lg ${state === tab.value ? "text-violet-500 border-b-2 border-violet-500" : ""} hover:text-violet-400 duration-200 drop-shadow-lg`}
+                            onClick={() => {
+                                setState(tab.value);
+                                router.push(`${url}${tab.value}`);
+                            }}
+                        >
+                            {tab.name}
+                        </button>
                     </li>
                 ))}
             </ul>
