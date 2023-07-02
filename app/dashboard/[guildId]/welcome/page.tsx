b1d64c62e425238aeeda562e77e5a9b67df08453
Luna <luni.siaa@gmail.com>
2023-07-03 00:30:29 +0200

    welcomer dash get data & add test button

--- app/dashboard/[guildId]/welcome/page.tsx
+++ app/dashboard/[guildId]/welcome/page.tsx
@@ -3,19 +3,16 @@
 import { useParams } from "next/navigation";
 import { useEffect, useState } from "react";
 
+import { errorStore } from "@/common/error";
 import { guildStore } from "@/common/guilds";
-import { userStore } from "@/common/user";
-import { widthStore } from "@/common/width";
 import SelectMenu from "@/components/inputs/SelectMenu";
-import { ApiV1GuildsChannelsGetResponse, RouteErrorResponse } from "@/typings";
+import { ApiV1GuildsChannelsGetResponse, ApiV1GuildsModulesWelcomeGetResponse, RouteErrorResponse } from "@/typings";
 
 export default function Home() {
-    const width = widthStore((w) => w);
-    const user = userStore((s) => s);
     const guild = guildStore((g) => g);
 
-    const [error, setError] = useState<string>();
     const [channels, setChannels] = useState<ApiV1GuildsChannelsGetResponse[]>([]);
+    const [welcome, setWelcome] = useState<ApiV1GuildsModulesWelcomeGetResponse>();
 
     const params = useParams();
 
@@ -36,26 +33,106 @@ export default function Home() {
                     }
                     default: {
                         setChannels([]);
-                        setError((response as unknown as RouteErrorResponse).message);
+                        errorStore.setState((response as unknown as RouteErrorResponse).message);
                         break;
                     }
                 }
 
             })
             .catch(() => {
-                setError("Error while fetching guilds");
+                errorStore.setState("Error while fetching channels");
+            });
+
+        fetch(`${process.env.NEXT_PUBLIC_API}/guilds/${params.guildId}/modules/welcome`, {
+            headers: {
+                authorization: localStorage.getItem("token") as string
+            }
+        })
+            .then(async (res) => {
+                const response = await res.json() as ApiV1GuildsModulesWelcomeGetResponse;
+                if (!response) return;
+
+                switch (res.status) {
+                    case 200: {
+                        setWelcome(response);
+                        break;
+                    }
+                    default: {
+                        setWelcome(undefined);
+                        errorStore.setState((response as unknown as RouteErrorResponse).message);
+                        break;
+                    }
+                }
+
+            })
+            .catch(() => {
+                errorStore.setState("Error while fetching channels");
             });
     }, []);
+
+    if (!welcome) return <></>;
 
     return (
         <div>
 
-            <SelectMenu
-                name="Channel"
-                url={`/guilds/${guild?.id}`}
-                items={channels.map((c) => { return { name: `#${c.name}`, value: c.id, error: c.missingPermissions.join(", ") }; })}
-                description="Select the channel where the welcome message should be send into"
-            />
+            <div className="flex gap-4">
+                <SelectMenu
+                    name="Channel"
+                    url={`/guilds/${guild?.id}/modules/welcome`}
+                    dataName={"channel"}
+                    items={channels.map((c) => { return { name: `#${c.name}`, value: c.id, error: c.missingPermissions.join(", ") }; })}
+                    description="Select the channel where the welcome message should be send into"
+                    defaultV={welcome.channel}
+                />
+
+                <button
+                    id="test-button"
+                    className="flex justify-center items-center bg-violet-500 hover:bg-violet-400 text-white py-2 px-4 rounded-md duration-200 drop-shadow-lg mt-8 h-12 w-32"
+                    onClick={() => {
+                        if (document.getElementById("test-button")?.classList.contains("cursor-not-allowed")) return;
+                        fetch(`${process.env.NEXT_PUBLIC_API}/guilds/${params.guildId}/modules/welcome/test`, {
+                            method: "POST",
+                            headers: {
+                                authorization: localStorage.getItem("token") as string
+                            }
+                        })
+                            .then(async (res) => {
+                                const response = await res.json() as ApiV1GuildsModulesWelcomeGetResponse;
+                                if (!response) return;
+
+                                switch (res.status) {
+                                    case 200: {
+                                        document.getElementById("test-button")?.classList.add("bg-green-500");
+                                        document.getElementById("test-button")?.classList.add("hover:bg-green-400");
+                                        document.getElementById("test-button")?.classList.add("cursor-not-allowed");
+                                        document.getElementById("test-button")?.classList.remove("bg-violet-500");
+                                        document.getElementById("test-button")?.classList.remove("hover:bg-violet-400");
+
+                                        setTimeout(() => {
+                                            document.getElementById("test-button")?.classList.remove("bg-green-500");
+                                            document.getElementById("test-button")?.classList.remove("hover:bg-green-400");
+                                            document.getElementById("test-button")?.classList.remove("cursor-not-allowed");
+                                            document.getElementById("test-button")?.classList.add("bg-violet-500");
+                                            document.getElementById("test-button")?.classList.add("hover:bg-violet-400");
+                                        }, 1_000 * 8);
+
+                                        break;
+                                    }
+                                    default: {
+                                        errorStore.setState((response as unknown as RouteErrorResponse).message);
+                                        break;
+                                    }
+                                }
+
+                            })
+                            .catch(() => {
+                                errorStore.setState("Error while fetching channels");
+                            });
+                    }}
+                >
+                    <span>Send Test</span>
+                </button>
+            </div>
 
         </div>
     );
--- typings.ts
+++ typings.ts
@@ -22,3 +22,51 @@ export interface ApiV1GuildsChannelsGetResponse {
     id: string;
     missingPermissions: string[]
 }
+
+interface GuildEmbed {
+    title: string | null;
+    description: string | null;
+    thumbnail: string | null;
+    image: string | null;
+    color: number;
+    footer: {
+        text: string | null;
+        icon_url: string | null;
+    }
+}
+
+export interface ApiV1GuildsModulesWelcomeGetResponse {
+    enabled: boolean;
+    channel?: string;
+    webhook?: string;
+
+    message: {
+        content?: string;
+        embed?: GuildEmbed
+    };
+
+    roles: string[];
+    pings: string[];
+    deleteAfter?: number;
+    restore: boolean;
+
+    dm: {
+        enabled: boolean;
+        message: {
+            content?: string;
+            embed?: GuildEmbed
+        };
+    };
+
+    reactions: {
+        welcomeMessage: string[],
+        firstMessage: string[],
+    };
+
+    card: {
+        enabled: boolean;
+        inEmbed: boolean;
+        background?: string;
+        textColor?: number;
+    }
+}
