bac1d68574094250f28ecf29aab9f202ffd4ce8c
Luna <luni.siaa@gmail.com>
2023-07-06 13:17:41 +0200

    add profile pages

---
+++ app/profile/layout.tsx
@@ -0,0 +1,51 @@
+"use client";
+import Image from "next/image";
+
+import { errorStore } from "@/common/error";
+import { userStore } from "@/common/user";
+import ErrorBanner from "@/components/Error";
+import { ListTab } from "@/components/List";
+
+export default function RootLayout({
+    children
+}: {
+    children: React.ReactNode
+}) {
+    const error = errorStore((e) => e);
+    const user = userStore((g) => g);
+
+    if (user === undefined && !error) return <></>;
+
+    return (
+        <div className="flex flex-col w-full">
+
+            {error && <ErrorBanner message={error} />}
+
+            <span className="text-lg flex items-center">
+                <Image src={user?.avatar || "https://cdn.waya.one/r/discord.png"} width={64} height={64} alt="Your profile picture" className="rounded-full h-14 w-14 mr-3" />
+                <div>
+                    <div className="text-xl dark:text-slate-200 text-slate-800 font-medium">{user ? `@${user.username}` : "Unknown User"}</div>
+                    <div className="text-sm">Manage your profile here</div>
+                </div>
+            </span>
+
+            <ListTab
+                tabs={[
+                    {
+                        name: "Overview",
+                        value: "/"
+                    },
+                    {
+                        name: "Rank",
+                        value: "/rank"
+                    }
+                ]}
+                url={"/profile"}
+                disabled={!user}
+            />
+
+            {children}
+
+        </div>
+    );
+}
---
+++ app/profile/page.tsx
@@ -0,0 +1,26 @@
+
+"use client";
+import Link from "next/link";
+import { BsDiscord } from "react-icons/bs";
+
+import { userStore } from "@/common/user";
+
+export default function Home() {
+    const user = userStore((s) => s);
+
+    return (
+        <div>
+
+            <div>Hey {user?.username}, thanks for testing out the early version of this bot :)</div>
+            <div>There will be more exciting stuff coming soon&trade;</div>
+
+            <div className="flex mt-4">
+                <Link href="/support" className="flex dark:bg-wamellow bg-wamellow-100 dark:hover:bg-wamellow-light hover:bg-wamellow-100-light dark:hover:text-white py-2 px-4 rounded-md duration-200 ">
+                    <BsDiscord className="relative top-1" />
+                    <span className="ml-2">Join our server for updates</span>
+                </Link>
+            </div>
+
+        </div>
+    );
+}
