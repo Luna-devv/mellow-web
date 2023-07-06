f56d8c982310d0852a1108e3c7423c30f62ec96b
Luna <luni.siaa@gmail.com>
2023-07-06 19:35:02 +0200

    Create ImageUrlInput.tsx

---
+++ components/inputs/ImageUrlInput.tsx
@@ -0,0 +1,134 @@
+import { FunctionComponent, useEffect, useState } from "react";
+import { TailSpin } from "react-loading-icons";
+
+import { RouteErrorResponse } from "@/typings";
+
+import TextInput from "./_TextInput";
+
+type Props = {
+    name: string;
+    url: string;
+    dataName: string;
+    disabled?: boolean;
+    description?: string;
+    __defaultState: string;
+};
+
+
+const ImageUrlInput: FunctionComponent<Props> = ({ name, url, dataName, disabled, description, __defaultState }) => {
+    const [state, setState] = useState<"LOADING" | "ERRORED" | "SUCCESS" | undefined>();
+    const [error, setError] = useState<string>();
+
+    const [value, setValue] = useState<string>("");
+    const [defaultvalue, setDefaultvalue] = useState<string>("");
+    const [imagestate, setImagestate] = useState<"ERRORED" | "SUCCESS" | undefined>(undefined);
+
+    useEffect(() => {
+        if (!defaultvalue) setDefaultvalue(__defaultState);
+        setValue(__defaultState);
+    }, [__defaultState]);
+
+    useEffect(() => {
+        if (value?.length) setImagestate(undefined);
+        else setImagestate("SUCCESS");
+    }, [value]);
+
+    useEffect(() => {
+        if (imagestate !== "SUCCESS" || defaultvalue === value) return;
+        setError(undefined);
+        setState("LOADING");
+
+        fetch(`${process.env.NEXT_PUBLIC_API}${url}`, {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json",
+                authorization: localStorage.getItem("token") as string
+            },
+            body: JSON.stringify({ [dataName]: value || null })
+        })
+            .then(async (res) => {
+                const response = await res.json();
+                if (!response) return;
+
+                switch (res.status) {
+                    case 200: {
+                        setValue(value);
+                        setDefaultvalue(value);
+
+                        setState("SUCCESS");
+                        setTimeout(() => setState(undefined), 1_000 * 8);
+                        break;
+                    }
+                    default: {
+                        setState("ERRORED");
+                        setError((response as unknown as RouteErrorResponse).message);
+                        break;
+                    }
+                }
+
+            })
+            .catch(() => {
+                setState("ERRORED");
+                setError("Error while updatung");
+            });
+
+    }, [imagestate]);
+
+    return (
+        <div className="relative">
+
+            <div className="flex items-center gap-2">
+                <span className="text-lg dark:text-slate-300 text-slate-700 font-medium">{name}</span>
+                {state === "LOADING" && <TailSpin stroke="#d4d4d4" strokeWidth={8} className="relative h-3 w-3 overflow-visible" />}
+            </div>
+
+
+            <div className="lg:flex mt-1 w-full gap-4">
+
+                <TextInput
+                    value={value}
+                    setValue={(v) => {
+                        setValue(v);
+                        setImagestate(undefined);
+                        setState(undefined);
+                    }}
+                    placeholder="Paste a direct image url..."
+                    max={256}
+                    description={description}
+                />
+
+                <div className="max-w-1/2 w-full">
+
+                    {value && imagestate !== "ERRORED" ?
+                        /* eslint-disable-next-line @next/next/no-img-element */
+                        <img src={value} alt="" className="w-full rounded-md aspect-[4/1]" onError={() => setImagestate("ERRORED")} onLoad={() => setImagestate("SUCCESS")} />
+                        :
+                        <div className={`w-full ${imagestate === "ERRORED" ? "border-red-500" : "border-wamellow"} border-2 rounded-md flex items-center justify-center aspect-[4/1]`}>
+                            {imagestate === "ERRORED" ?
+                                <div className="text-red-400 m-4">
+                                    <div className="font-medium">Enter a <span className="underline underline-red-400">valid</span> image url!</div>
+                                    <div className="text-xs">
+                                        <div>Recommended resolution: 1024x256</div>
+                                        <div>Recommended type: .jpg</div>
+                                    </div>
+                                </div>
+                                :
+                                <span className="text-neutral-400">Enter a image url to preview</span>
+                            }
+                        </div>
+                    }
+
+                </div>
+
+            </div>
+
+            <div className="flex">
+                {(error || state === "ERRORED") && <div className="ml-auto text-red-500 text-sm">{error || "Unknown error while saving"}</div>}
+                {state === "SUCCESS" && <div className="ml-auto text-green-500 text-sm">{"Saved"}</div>}
+            </div>
+
+        </div>
+    );
+};
+
+export default ImageUrlInput;
