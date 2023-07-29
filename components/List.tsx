f94b8bab18c29e51386494aa83b53eae4d199de3
Luna <luni.siaa@gmail.com>
2023-07-29 02:30:47 +0200

    ListTab add query functionality

--- components/List.tsx
+++ components/List.tsx
@@ -1,4 +1,5 @@
-import { usePathname, useRouter } from "next/navigation";
+"use client";
+import { usePathname, useRouter, useSearchParams } from "next/navigation";
 import React, { FunctionComponent } from "react";
 
 interface Props {
@@ -8,11 +9,13 @@ interface Props {
         value: string;
     }[];
     url: string;
+    searchParamName?: string;
     disabled: boolean;
 }
 
-export const ListTab: FunctionComponent<Props> = ({ tabs, url, disabled }) => {
+export const ListTab: FunctionComponent<Props> = ({ tabs, url, searchParamName, disabled }) => {
     const path = usePathname();
+    const params = useSearchParams();
     const router = useRouter();
 
     return (
@@ -21,8 +24,20 @@ export const ListTab: FunctionComponent<Props> = ({ tabs, url, disabled }) => {
                 {tabs.map((tab) => (
                     <li className="mr-2" key={tab.name}>
                         <button
-                            className={`inline-block p-3 border-b-2 border-transparent rounded-t-lg ${path.endsWith(`${url}${tab.value === "/" ? "" : tab.value}`) && "text-violet-500 border-b-2 border-violet-500"} hover:text-violet-400 duration-200 ${disabled && "cursor-not-allowed"}`}
-                            onClick={() => { if (!disabled) router.push(`${url}${tab.value}`); }}
+                            className={`inline-block p-3 border-b-2 border-transparent rounded-t-lg ${(searchParamName ? (tab.value ? params.get(searchParamName) === tab.value : !tab.value && !params.get(searchParamName)) : path.endsWith(`${url}${tab.value === "/" ? "" : tab.value}`)) && "text-violet-500 border-b-2 border-violet-500"} hover:text-violet-400 duration-200 ${disabled && "cursor-not-allowed"}`}
+                            onClick={() => {
+                                if (disabled) return;
+
+                                if (searchParamName) {
+                                    const newparams = new URLSearchParams();
+
+                                    if (tab.value) newparams.append(searchParamName, tab.value);
+                                    else newparams.delete(searchParamName);
+
+                                    router.push(`${url}?${newparams.toString()}`);
+                                }
+                                else router.push(`${url}${tab.value}`);
+                            }}
                         >
                             {tab.name}
                         </button>
