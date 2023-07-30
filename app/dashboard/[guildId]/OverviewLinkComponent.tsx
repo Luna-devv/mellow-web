d4355bfeb42777879e565222a367fd3d56b63dd6
Luna <luni.siaa@gmail.com>
2023-07-30 14:18:07 +0200

    OverviewLinkComponent add lightmode

--- app/dashboard/[guildId]/OverviewLinkComponent.tsx
+++ app/dashboard/[guildId]/OverviewLinkComponent.tsx
@@ -2,25 +2,27 @@ import Link from "next/link";
 import { FunctionComponent } from "react";
 import { HiArrowRight, HiChartBar } from "react-icons/hi";
 
-const OverviewLinkComponent: FunctionComponent<{ title: string, message: string, url: string }> = ({ title, message, url }) => {
+const OverviewLinkComponent: FunctionComponent<{ title: string, message: string, url: string, className?: string }> = ({ title, message, url, className }) => {
     return (
-        <Link href={url}>
-            <div className="w-full text-neutral-100 bg-wamellow py-4 px-5 mb-6 rounded-md outline-violet-500 hover:outline flex gap-2 group/item ">
+        <div className={className}>
+            <Link href={url}>
+                <div className="w-full dark:text-neutral-100 text-neutral-900 dark:bg-wamellow bg-wamellow-100 py-4 px-5 mb-6 rounded-md outline-violet-500 hover:outline flex gap-2 group/item ">
 
-                <div>
+                    <div>
 
-                    <div className="flex gap-2 items-center">
-                        <div className="text-xl font-semibold">{title}</div>
-                        <HiArrowRight className="group-hover/item:ml-[6px] duration-300" style={{ height: 20, width: 20 }} />
+                        <div className="flex gap-2 items-center">
+                            <div className="text-xl font-semibold">{title}</div>
+                            <HiArrowRight className="group-hover/item:ml-[6px] duration-300" style={{ height: 20, width: 20 }} />
+                        </div>
+
+                        <div className="text-sm">{message}</div>
                     </div>
 
-                    <div className="text-sm">{message}</div>
+                    <HiChartBar className="ml-auto text-violet-400 group-hover/item:text-violet-500 duration-300" style={{ height: "3rem", width: "3rem" }} />
+
                 </div>
-
-                <HiChartBar className="ml-auto text-violet-400 group-hover/item:text-violet-500 duration-300" style={{ height: "3rem", width: "3rem" }} />
-
-            </div>
-        </Link>
+            </Link>
+        </div>
     );
 };
 
