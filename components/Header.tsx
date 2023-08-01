d9f57bf4f26c6f30aab11caccf5ec2cf6d2d5a24
Luna <luni.siaa@gmail.com>
2023-08-02 00:06:38 +0200

    move to local prod

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,6 +1,5 @@
 "use client";
 
-import { Analytics } from "@vercel/analytics/react";
 import { Inter } from "next/font/google";
 import Image from "next/image";
 import Link from "next/link";
@@ -135,7 +134,6 @@ const Header: FunctionComponent<Props> = ({ children }) => {
 
     return (
         <html lang="en" className="flex justify-center min-h-full max-w-full overflow-x-hidden">
-            <Analytics />
 
             {/* make this relative to support the footer, will break the header (lol) */}
             <body className={`${inter.className} w-full max-w-6xl min-h-full`}>
