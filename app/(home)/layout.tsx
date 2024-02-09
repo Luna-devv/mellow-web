c5fb3d6b6e1e55205488664f470397daeb2f8ba6
Luna <luni.siaa@gmail.com>
2024-02-09 01:40:58 +0100

    change footer links

--- app/(home)/layout.tsx
+++ app/(home)/layout.tsx
@@ -1,4 +1,3 @@
-import { readFile } from "fs/promises";
 import Image from "next/image";
 import Link from "next/link";
 import { BiCopyright, BiLogoGithub, BiLogoGmail, BiLogoTiktok, BiLogoTwitter, BiLogoYoutube } from "react-icons/bi";
@@ -9,6 +8,7 @@ import { SiKofi } from "react-icons/si";
 import TopggIcon from "@/components/icons/topgg";
 import { ServerButton } from "@/components/server-button";
 import BlahajPic from "@/public/blahaj.webp";
+import LunaPic from "@/public/luna.webp";
 import PronounsPic from "@/public/pronouns-bot.webp";
 
 export default async function RootLayout({
@@ -17,7 +17,7 @@ export default async function RootLayout({
     children: React.ReactNode
 }) {
 
-    const version = (await readFile(".git/FETCH_HEAD")).toString();
+    // const version = (await readFile(".git/FETCH_HEAD")).toString();
 
     return (
         <div className="w-full">
@@ -38,20 +38,24 @@ export default async function RootLayout({
                         <span className="flex gap-1 items-center">
                             <BiCopyright />
                             <span>
-                                <Link href="https://waya.one" className="hover:underline">Waya {new Date(1635609600000).getFullYear()} - {new Date().getFullYear()}</Link>,
+                                <Link href="/" className="hover:underline">Wamellow {new Date(1635609600000).getFullYear()} - {new Date().getFullYear()}</Link>,
                                 not affiliated with Discord Inc.
                             </span>
                         </span>
                         <span className="flex gap-1 items-center">
                             <HiCube />
-                            <span>
-                                Version {version.toString().slice(0, 7)} by <Link className="hover:underline" href="https://lunish.nl" aria-label="lunish.nl developer">lunish.nl</Link>
+                            <span className="flex gap-0.5 items-center">
+                                {/* Version {version.toString().slice(0, 7)} by */}
+                                <span>
+                                    Made by <Link className="hover:underline" href="/support" aria-label="lunish.nl developer">Luna</Link>
+                                </span>
+                                <Image src={LunaPic} alt="mwlica" width={16} height={16} className="h-4 w-4 rounded-full ml-0.5" />
                             </span>
                         </span>
                     </div>
 
                     <div className="ml-auto svg-max flex flex-wrap items-center gap-2 mt-2 md:mt-0">
-                        <Link href="https://tiktok.com/@waya.one" className="text-neutral-400 hover:dark:text-neutral-300 hover:text-neutral-700 duration-200 h-6 w-6" aria-label="Wamellow on TikTok">
+                        <Link href="https://tiktok.com/@wamellow.com" className="text-neutral-400 hover:dark:text-neutral-300 hover:text-neutral-700 duration-200 h-6 w-6" aria-label="Wamellow on TikTok">
                             <BiLogoTiktok />
                         </Link>
                         <Link href="https://youtube.com/@wayabot" className="text-neutral-400 hover:dark:text-neutral-300 hover:text-neutral-700 duration-200 h-6 w-6" aria-label="Wamellow on YouTube">
@@ -63,13 +67,13 @@ export default async function RootLayout({
                         <Link href="https://github.com/Luna-devv" className="text-neutral-400 hover:dark:text-neutral-300 hover:text-neutral-700 duration-200 h-6 w-6" aria-label="Wamellow's developers on GitHub">
                             <BiLogoGithub />
                         </Link>
-                        <Link href="mailto:support@waya.one" className="text-neutral-400 hover:dark:text-neutral-300 hover:text-neutral-700 duration-200 h-6 w-6" aria-label="Contaxt Wamellow via email">
+                        <Link href="mailto:support@waya.one" className="text-neutral-400 hover:dark:text-neutral-300 hover:text-neutral-700 duration-200 h-6 w-6" aria-label="Contact Wamellow via email">
                             <BiLogoGmail />
                         </Link>
                         <Link href="https://lunish.nl/kofi" className="text-neutral-400 hover:dark:text-neutral-300 hover:text-neutral-700 duration-200 h-[22px] w-[22px]" aria-label="Support Wamellow's developers monetarily on Kofi">
                             <SiKofi />
                         </Link>
-                        <Link href="https://top.gg/bot/1125449347451068437" className="text-neutral-400 hover:text-[#ff3366] duration-200 h-6 w-6" aria-label="Wamellow on top.gg">
+                        <Link href="https://top.gg/bot/1125449347451068437/vote" className="text-neutral-400 hover:text-[#ff3366] duration-200 h-6 w-6" aria-label="Wamellow on top.gg">
                             <TopggIcon />
                         </Link>
                     </div>
