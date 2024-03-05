c79709cda3e6531b28540c7b10b96afad194b31e
Luna <luna@waya.one>
2024-03-05 10:51:07 +0100

    fix unknown guild og image

--- app/leaderboard/[guildId]/open-graph.png/route.tsx
+++ app/leaderboard/[guildId]/open-graph.png/route.tsx
@@ -5,7 +5,9 @@ import { ImageResponse } from "next/og";
 import { NextRequest } from "next/server";
 
 import { getGuild } from "@/lib/api";
+import { intl } from "@/utils/numbers";
 import { truncate } from "@/utils/truncate";
+import { getCanonicalUrl } from "@/utils/urls";
 
 import { getTopMembers } from "../api";
 import Icon from "../icon.component";
@@ -23,7 +25,7 @@ export async function GET(
     const guild = await getGuild(params.guildId);
     const members = await getTopMembers(params.guildId, { page: 1, type: "messages" });
 
-    const intl = new Intl.NumberFormat("en", { notation: "standard" });
+    const guildExists = guild && "id" in guild;
 
     return new ImageResponse(
         (
@@ -33,34 +35,36 @@ export async function GET(
                 </div>
                 <div tw="flex mb-3 items-center">
                     <img
-                        src={guild?.icon ? `https://cdn.discordapp.com/icons/${guild.id}/${guild.icon}.png` : "/discord.png"}
+                        src={guildExists && guild.icon ? `https://cdn.discordapp.com/icons/${guild.id}/${guild.icon}.png` : getCanonicalUrl("discord.png")}
                         tw="h-20 w-20 rounded-2xl relative bottom-3 mr-5"
                         alt=""
                     />
-                    <div style={{ fontWeight: 800, fontSize: "5rem" }}>{truncate(guild?.name || "unknown", 20)}</div>
+                    <div style={{ fontWeight: 800, fontSize: "5rem" }}>{truncate(guildExists ? guild.name : "unknown", 20)}</div>
                 </div>
                 <div tw="text-4xl text-gray-500 mb-42" style={{ fontWeight: 500 }}>Explore the vibrant community dynamics</div>
 
 
-                <div tw="flex justify-between">
-                    {members.slice(0, 3).map((member) => (
-                        <div key={member.id} tw="flex flex-col">
-                            <div tw="flex items-center mb-2 text-5xl" style={{ fontWeight: 600 }}>
-                                <img
-                                    src={member?.avatar ? `https://cdn.discordapp.com/avatars/${member.id}/${member.avatar}.png` : "/discord.png"}
-                                    tw="h-14 w-14 rounded-full mb-2.5 mr-4"
-                                    alt=""
-                                />
-                                {truncate(member.globalName || member.username || "unknown", 10)}
-                            </div>
+                {Array.isArray(members) &&
+                    <div tw="flex justify-between">
+                        {members.slice(0, 3).map((member) => (
+                            <div key={member.id} tw="flex flex-col">
+                                <div tw="flex items-center mb-2 text-5xl" style={{ fontWeight: 600 }}>
+                                    <img
+                                        src={member?.avatar ? `https://cdn.discordapp.com/avatars/${member.id}/${member.avatar}.png` : getCanonicalUrl("discord.png")}
+                                        tw="h-14 w-14 rounded-full mb-2.5 mr-4"
+                                        alt=""
+                                    />
+                                    {truncate(member.globalName || member.username || "unknown", 10)}
+                                </div>
 
-                            <div tw="text-3xl text-gray-400 flex text-3xl" style={{ fontWeight: 500 }}>
-                                <span tw="mr-2">{intl.format(member.activity.messages)}</span>
-                                <Icon type={type as "messages"} />
+                                <div tw="text-3xl text-gray-400 flex text-3xl" style={{ fontWeight: 500 }}>
+                                    <span tw="mr-2">{intl.format(member.activity.messages)}</span>
+                                    <Icon type={type as "messages"} />
+                                </div>
                             </div>
-                        </div>
-                    ))}
-                </div>
+                        ))}
+                    </div>
+                }
 
             </div>
         ),
