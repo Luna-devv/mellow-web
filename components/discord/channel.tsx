abd63fbf24c8907da697f4071f2da93df6e99ae4
Luna <luni.siaa@gmail.com>
2024-04-15 18:51:57 +0200

    add leaderboard permission warnings

--- components/discord/channel.tsx
+++ components/discord/channel.tsx
@@ -1,25 +1,36 @@
 import React from "react";
 import { HiHashtag, HiVolumeUp } from "react-icons/hi";
 
+import cn from "@/utils/cn";
+
 interface Props {
     type: "text" | "voice";
     name: string;
+    isTruncated: boolean;
     children?: React.ReactNode;
 }
 
 export default function DiscordChannel({
     type,
     name,
+    isTruncated,
     children
 }: Props) {
     return (
-        <div className="text-medium text-neutral-400">
+        <div
+            className={cn(
+                "text-medium text-neutral-400 w-full",
+                isTruncated && "max-w-[calc(100%-2rem)]"
+            )
+            }>
             <span className="flex items-center gap-1">
                 {type === "text"
                     ? <HiHashtag />
                     : <HiVolumeUp />
                 }
-                {name}
+                <span className="w-full truncate">
+                    {name}
+                </span>
             </span>
             {children &&
                 <div className="ml-6 mt-2 flex flex-col gap-3">
