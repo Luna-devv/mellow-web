779044a1624a37c6ed3afc638a7951bef7e1a68b
Luna <luna@waya.one>
2024-11-26 18:54:10 +0100

    use new skeleton ui

--- app/leaderboard/[guildId]/loading.tsx
+++ app/leaderboard/[guildId]/loading.tsx
@@ -1,21 +1,21 @@
-import { ClientSkeleton } from "@/components/client";
+import { Skeleton } from "@/components/ui/skeleton";
 
 export default function Loading() {
     return new Array(20).fill(null).map((_, i) => (
         <div
-            key={"leaderboardLoading-" + i}
+            key={"leaderboard-loading-" + i}
             className="mb-4 rounded-md p-3 flex items-center dark:bg-wamellow bg-wamellow-100 w-full"
         >
-            <ClientSkeleton className="rounded-full w-12 h-12 mr-3" />
+            <Skeleton className="rounded-full w-12 h-12 mr-3" />
 
             <div className="flex flex-col gap-2 mt-0.5">
-                <ClientSkeleton className="h-5 w-28 rounded-full" />
-                <ClientSkeleton className="h-3 w-20 rounded-full" />
+                <Skeleton className="h-5 w-28 rounded-full" />
+                <Skeleton className="h-3 w-20 rounded-full" />
             </div>
 
-            <ClientSkeleton className="ml-auto h-8 w-14 rounded-lg" />
+            <Skeleton className="ml-auto h-8 w-14 rounded-lg" />
 
-            <ClientSkeleton className="rounded-full w-12 h-12 ml-3" />
+            <Skeleton className="rounded-full w-12 h-12 ml-3" />
         </div>
     ));
 }
