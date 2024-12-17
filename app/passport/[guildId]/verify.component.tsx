ea3b9b2aa59a884d8f2c427c498592247f467954
Luna <luna@waya.one>
2024-12-17 21:11:59 +0100

    create captcha verification hook

--- app/passport/[guildId]/verify.component.tsx
+++ app/passport/[guildId]/verify.component.tsx
@@ -1,21 +1,15 @@
 "use client";
+
 import { Button } from "@nextui-org/react";
 import Link from "next/link";
-import { useEffect, useRef, useState } from "react";
 import { BsDiscord } from "react-icons/bs";
 import { HiExclamation, HiFingerPrint, HiLockClosed } from "react-icons/hi";
 
 import { userStore } from "@/common/user";
 import ImageReduceMotion from "@/components/image-reduce-motion";
-import { GT4Init } from "@/lib/gt4";
 import type { ApiV1GuildsGetResponse } from "@/typings";
+import { State, useCaptcha } from "@/utils/captcha";
 import { cn } from "@/utils/cn";
-
-enum State {
-    Idle = 0,
-    Loading = 1,
-    Success = 2
-}
 
 interface Props {
     guild: ApiV1GuildsGetResponse;
@@ -24,107 +18,8 @@ interface Props {
 export default function VerifyComponent({ guild }: Props) {
     const user = userStore((s) => s);
 
-    const btnRef = useRef<HTMLButtonElement>(null);
-
-    const [state, setState] = useState<State>(State.Idle);
-    const [error, setError] = useState<string | null>(null);
-
-    useEffect(() => {
-        if (!user?.id) return;
-        const { init } = GT4Init();
-
-        init(
-            {
-                captchaId: process.env.NEXT_PUBLIC_CAPTCHA_ID,
-                product: "bind",
-                // riskType: "icon",
-                hideSuccess: true,
-                userInfo: user?.id
-            },
-            handlerForBind
-        );
-
-    }, [user]);
-
-    // @ts-expect-error GeeTest types suck
-    function handlerForBind(c) {
-
-        const button = btnRef.current;
-        let isReady = false;
-
-        c.onReady(() => {
-            isReady = true;
-        });
-
-        button?.addEventListener("click", function () {
-            if (!isReady) return;
-            if (state === State.Success) return;
-            setState(State.Idle);
-            setError(null);
-
-            c.showCaptcha();
-        });
-
-        c.onSuccess(async () => {
-            setState(State.Loading);
-
-            const result = c.getValidate();
-            const res = await fetch(`${process.env.NEXT_PUBLIC_API}/guilds/${guild.id}/passport-verification/captcha`, {
-                method: "POST",
-                credentials: "include",
-                headers: {
-                    "Content-Type": "application/json"
-                },
-                body: JSON.stringify(result)
-            })
-                .catch(() => null);
-
-            switch (res?.status) {
-                case 200:
-                    setState(State.Success);
-                    break;
-                default:
-                    setState(State.Idle);
-                    setError((await res?.json())?.message || "Unknown server error");
-                    break;
-            }
-
-        });
-
-        c.onError((err: string) => {
-            c.reset();
-            setState(State.Idle);
-            setError(`${err}` || "Unknown captcha error");
-        });
-
-        c.onClose(() => {
-            c.reset();
-            setState(State.Idle);
-        });
-
-        c.onFail(async () => {
-            setState(State.Loading);
-
-            const res = await fetch(`${process.env.NEXT_PUBLIC_API}/guilds/${guild.id}/passport-verification/captcha?captcha-failed=true`, {
-                method: "POST",
-                credentials: "include"
-            })
-                .catch(() => null);
-
-            switch (res?.status) {
-                case 409:
-                    setState(State.Idle);
-                    break;
-                default:
-                    c.destroy();
-                    setState(State.Idle);
-                    setError((await res?.json())?.message || "Unknown server error");
-                    break;
-            }
-
-        });
-
-    }
+    const url = `/guilds/${guild.id}/passport-verification/captcha` as const;
+    const { state, error, button } = useCaptcha(url, user?.id);
 
     return (
         <div className="flex flex-col gap-3 w-full mt-4">
@@ -154,7 +49,7 @@ export default function VerifyComponent({ guild }: Props) {
                     </Button>
                     :
                     <Button
-                        ref={btnRef}
+                        ref={button}
                         color={error ? "danger" : state === State.Success ? "success" : "secondary"}
                         className={cn(error && "cursor-not-allowed", state === State.Success && "cursor-not-allowed", "font-medium w-full")}
                         isDisabled={!!error || state === State.Success}
---
+++ utils/captcha.ts
@@ -0,0 +1,116 @@
+import { useEffect, useRef, useState } from "react";
+
+import { GT4Init } from "@/lib/gt4";
+
+export enum State {
+    Idle = 0,
+    Loading = 1,
+    Success = 2
+}
+
+export function useCaptcha(path: string, userId?: string) {
+    const [state, setState] = useState<State>(State.Idle);
+    const [error, setError] = useState<string | null>(null);
+
+    const button = useRef<HTMLButtonElement | null>(null);
+
+    useEffect(() => {
+        if (!userId) return;
+        const { init } = GT4Init();
+
+        init(
+            {
+                captchaId: process.env.NEXT_PUBLIC_CAPTCHA_ID,
+                product: "bind",
+                hideSuccess: true,
+                userInfo: userId
+            },
+            handlerForBind
+        );
+
+        // @ts-expect-error GeeTest types suck
+        function handlerForBind(captcha) {
+            const btn = button.current;
+            let isReady = false;
+
+            captcha.onReady(() => {
+                isReady = true;
+            });
+
+            btn?.addEventListener("click", () => {
+                if (!isReady || state === State.Success) return;
+
+                setState(State.Idle);
+                setError(null);
+
+                captcha.showCaptcha();
+            });
+
+            captcha.onSuccess(async () => {
+                setState(State.Loading);
+
+                const result = captcha.getValidate();
+                const res = await fetch(process.env.NEXT_PUBLIC_API + path, {
+                    method: "POST",
+                    credentials: "include",
+                    headers: {
+                        "Content-Type": "application/json"
+                    },
+                    body: JSON.stringify(result)
+                })
+                    .catch(() => null);
+
+                if (!res?.ok) {
+                    captcha.destroy();
+                    setState(State.Idle);
+
+                    const { message } = res
+                        ? await res.json()
+                        : { message: "Unknown server error" };
+
+                    setError(message);
+                    return;
+                }
+
+                setState(State.Success);
+            });
+
+            captcha.onFail(async () => {
+                setState(State.Loading);
+
+                const res = await fetch(`${process.env.NEXT_PUBLIC_API + path}?captcha-failed=true`, {
+                    method: "POST",
+                    credentials: "include"
+                });
+
+                if (res.status === 409) {
+                    setState(State.Idle);
+                    return;
+                }
+
+                captcha.destroy();
+                setState(State.Idle);
+
+                const { message } = res
+                    ? await res.json()
+                    : { message: "Unknown server error" };
+
+                setError(message);
+                return;
+            });
+
+            captcha.onError((err: string) => {
+                captcha.reset();
+                setState(State.Idle);
+                setError(err || "Unknown captcha error");
+            });
+
+            captcha.onClose(() => {
+                captcha.reset();
+                setState(State.Idle);
+            });
+        }
+    }, [path, userId]);
+
+    return { state, error, button };
+}
