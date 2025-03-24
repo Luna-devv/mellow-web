4f4b0484ee68b0cfe28cc8d916645f5e2de97eb4
Luna <luni.siaa@gmail.com>
2025-03-24 18:06:51 +0100

    `/bluesky` redirect to bsky post on bluesky

--- app/(dynamic-assets)/bluesky/route.ts
+++ app/(dynamic-assets)/bluesky/route.ts
@@ -3,9 +3,15 @@ import { redirect } from "next/navigation";
 export const revalidate = 691200; // 8 days
 
 export function GET(request: Request) {
-    if (!request.headers.get("user-agent")?.includes("Discordbot/2.0")) {
-        redirect("https://www.reddit.com/r/BlueskySocial/comments/1jbjmc4/like_bluesky_posts_inside_of_discord_with_wamellow/");
+    const agent = request.headers.get("user-agent");
+
+    if (agent?.includes("Discordbot")) {
+        redirect("/wamellow-bluesky-like.webp");
     }
 
-    redirect("/wamellow-bluesky-like.webp");
+    if (agent?.includes("Bluesky Card")) {
+        redirect("https://bsky.app/profile/did:plc:jjtrsxiaw4amuujelxrxy7by/post/3lketwsywwc2r");
+    }
+
+    redirect("https://www.reddit.com/r/BlueskySocial/comments/1jbjmc4/like_bluesky_posts_inside_of_discord_with_wamellow/");
 }
