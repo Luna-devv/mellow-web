e8763481b06fc57ead507ce1c9b8c07a0951a092
Luna <luni.siaa@gmail.com>
2025-06-22 21:30:00 +0200

    add translate and datalix redirects

--- app/[pathname]/page.tsx
+++ app/[pathname]/page.tsx
@@ -4,7 +4,6 @@ interface Props {
     params: Promise<{ pathname: string; }>;
 }
 
-const fetchOptions = { next: { revalidate: 60 * 60 } };
 const utm = "?utm_source=wamellow.com&utm_medium=redirect";
 
 export default async function Home({ params }: Props) {
@@ -13,7 +12,6 @@ export default async function Home({ params }: Props) {
     switch (pathname) {
         case "support": return redirect("https://discord.com/invite/DNyyA2HFM9");
         case "vote": return redirect("https://top.gg/bot/1125449347451068437/vote" + utm);
-        case "wumpus": return redirect("https://wumpus.store/bot/1125449347451068437" + utm);
         case "invite":
         case "add":
         case "get": return redirect("/login?invite=true");
@@ -23,13 +21,10 @@ export default async function Home({ params }: Props) {
         case "ai": return redirect("/ai-gallery");
         case "tts": return redirect("https://youtube.com/watch?v=NS5fZ1ltovE");
         case "disable-commands": return redirect("https://youtube.com/watch?v=ehc0_whydu8");
-        case "youtube": {
-            const res = await fetch("http://100.65.0.1:5001/?channel_id=UClWBeVcz5LUmcCN1gHG_GCg", fetchOptions)
-                .then((res) => res.json())
-                .catch(() => null) as { videoUrl: string; } | null;
-
-            return redirect(res?.videoUrl || "https://www.youtube.com/channel/UClWBeVcz5LUmcCN1gHG_GCg");
-        }
+        case "youtube": return redirect("https://www.youtube.com/channel/UClWBeVcz5LUmcCN1gHG_GCg");
+        case "translate": return redirect("https://crowdin.com/project/wamellow");
+        case "ref": return redirect("https://datalix.eu/a/luna" + utm);
+        case "deref": return redirect("https://datalix.de/a/luna" + utm);
     }
 
     notFound();
